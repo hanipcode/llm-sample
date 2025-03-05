c2f68a7c12e50259ec1ed860f1770f759558a9ff
Muhammad Hanif <hanifeij@yahoo.co.id>
2025-03-05 21:59:48 +0700

    feat: 3rd iteration

--- main.py
+++ main.py
@@ -25,11 +25,25 @@ def get_base_message():
     return [
         {
             "role": "system",
-            "content": "You are an AI system.  you are a naming expert that have knowledge of naming pattern around the world, you have expertise and knowledge about  name because you know the culture of different countries, the prefix usually used, and the honorifics. you should also account for a typo, you should also account for a repeated name because first name and last name is required, also ignore the text casing. also you should remember about abbreviation both in name and honorifics, for example SAUDARA / SODARA can be SDR and Caesar can be C.",
+            "content": """You are an AI system.  
+            you are a naming expert that have knowledge of naming pattern around the world, you can check whether two person is the same person
+            you are knowing honorifics and titles used around the world
+            here are pre steps you should follow for both names:
+            1. make it lowercase
+            2. remove all punctuation and special character
+            3. expand all abbreviations in the text, be it abbreviation of name, and abbreviation of honorifics and abbreviation of title
+            3. strip all common honorifics you know
+            4. strip all title be it religious title, education title, or job title
+            5. strip all alias and word that is most likely not a name for example like HANIF TRADER HANDAL is same with HANIF
+            6. If you found a word with only 1 Character, you should try to relate it to other name. for example name a is Wendy B, and name b is Wendy Bhagaskara, mostlikely B is Bhagaskara
+            7. try to calculate edit distance between word and expand the most likely abbreviated name, like Andri and Andriyanto most likely the same because have close edit distance 
+
+            THIS RULE IS VERY IMPORTANT AND YOU SHOULD FOLLOW IT
+            """,
         },
         {
             "role": "system",
-            "content": 'you will be given two name by a user, you should decide whether the two given name most likely are the same person. you should return the response as a json with format `{"isSamePerson": boolean}` isSamePerson is whether the person same or not in boolean true/false',
+            "content": 'you will be given two name by a user, you should decide whether the two given name most likely are the same person based on the rule above. you should return the response as a json with format `{"isSamePerson": boolean}` isSamePerson is whether the person same or not in boolean true/false',
         },
         {
             "role": "system",
